fc2cf3248f103d755b4c6195fe729609c7c90ee8
Gonzalo Olima <gonzaloolima68@gmail.com>
2026-06-15 20:35:34 -0300

    creacion de gitignore y archivo

---
+++ LectorDirectorio/archivo.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Archivo{
+    private string name;
+    private double tamanio;
+    private DateTime date;
+
+    public string Name{ get ; set; }
+    public double Tamanio{get ;set;}
+    public DateTime Date{get ; set;}
+
+}

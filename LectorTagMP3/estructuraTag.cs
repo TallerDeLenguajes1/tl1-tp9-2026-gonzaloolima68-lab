7eaac468382c69ad4d4157366ae79540f323dee4
Gonzalo Olima <gonzaloolima68@gmail.com>
2026-06-28 11:43:20 -0300

    movemos el archivo estructuraTag a la carpeta Lector

---
+++ LectorTagMP3/estructuraTag.cs
@@ -0,0 +1,19 @@
+namespace EstructuraTag
+{
+    public class tagID3v1
+    {
+        private string titulo;
+        private string artista;
+        private string albun;
+        private DateTime año;
+        private string comentario;
+        private string genero;
+
+        public string Titulo {get=>titulo; set=>titulo=value;}
+        public string Artista{get=>artista; set=>artista=value;}
+        public string Albun{get=>albun; set=>albun=value;}
+        public DateTime Año{get=>año; set=>año=value;}
+        public string Comentario{get=>comentario; set=>comentario=value;}
+        public string Genero{get=>genero; set=>genero=value;}
+    }
+}
